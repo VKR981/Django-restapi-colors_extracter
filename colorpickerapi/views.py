95f4888e671a7a8696310b8225c3d82a735aa383
VKR981 <66425536+VKR981@users.noreply.github.com>
2020-09-29 14:46:57 +0530

    added get method and test results file

--- colorpickerapi/views.py
+++ colorpickerapi/views.py
@@ -6,6 +6,8 @@ from rest_framework import viewsets, permissions, views
 from rest_framework.decorators import parser_classes
 from rest_framework.parsers import MultiPartParser, FileUploadParser
 from PIL import Image
+import requests
+from io import BytesIO
 from rest_framework.response import Response
 import extcolors
 
@@ -19,16 +21,32 @@ class imageapi(views.APIView):
     parser_classes = [MultiPartParser]
 
     def post(self, request):
-        print(request.data)
+
         try:
-            colors = processimg(request.data.getlist('image')[0])
+            colors = processimg(fp=request.data.getlist('image')[0])
         except:
             return Response({'error': 'no file found, make sure id/key is set to "image"'})
 
-        return Response({'colors': colors})
+        return Response({
+                        'logo_border': colors[1],
+                        'dominant_color': colors[0]
+                        })
+
+    def get(self, request):
+        img_url = request.query_params.get('src')
+        colors = processimg(url=img_url)
+        return Response({
+                        'logo_border': colors[1],
+                        'dominant_color': colors[0]
+                        })
 
 
-def processimg(fp):
-    im = Image.open(fp)
+def processimg(fp=None, url=None):
+    if url:
+        response = requests.get(url)
+        im = Image.open(BytesIO(response.content))
+    else:
+        im = Image.open(fp)
     colors, pixel_count = extcolors.extract_from_image(im)
-    return (colors[0][0], colors[1][0])
+
+    return ('#%02x%02x%02x' % colors[0][0], '#%02x%02x%02x' % colors[1][0])
---
+++ test_outputs.md
@@ -0,0 +1,16 @@
+{
+    "logo_border": "#8b6d24",
+    "dominant_color": "#fed9e0"
+}
+{
+    "logo_border": "#6d6e71",
+    "dominant_color": "#eff4f6"
+}
+{
+    "logo_border": "#ffffff",
+    "dominant_color": "#1a1a1a"
+}
+{
+    "logo_border": "#020202",
+    "dominant_color": "#98fd03"
+}
